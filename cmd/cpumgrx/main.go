0011cf3519009d4ac7b14efbc5547f2fdb792553
Francesco Romani <fromani@redhat.com>
2021-02-11 14:12:45 +0100

    cpumgrx: read all the parameters
    we need quite some configuration/parameters, and with
    this patch we make sure to read them all.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cmd/cpumgrx/main.go
+++ cmd/cpumgrx/main.go
@@ -17,17 +17,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"flag"
+	cadvisorapi "github.com/google/cadvisor/info/v1"
 	"github.com/spf13/pflag"
 
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/kubelet/cm/cpuset"
+	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager"
 
 	"github.com/fromanirh/cpumgrx/pkg/cpumgrx"
+	"github.com/fromanirh/cpumgrx/pkg/tmutils"
 )
+
+//	MachineInfo    *cadvisorapi.MachineInfo
 
 func main() {
 	// Add klog flags
@@ -35,11 +43,103 @@ func main() {
 	// Add flags registered by imported packages
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 
-	params := cpumgrx.Params{}
+	var podSpecPath string
+	var policyName string
+	var rawHint string
+	var rawReservedCPUs string
+	var machineInfoPath string
+	pflag.StringVarP(&rawReservedCPUs, "reserved-cpus", "R", "0", "set reserved CPUs")
+	pflag.StringVarP(&rawHint, "hint", "H", "", "set topology manager hint")
+	pflag.StringVarP(&machineInfoPath, "machine-info", "M", "", "machine info path")
+	pflag.StringVarP(&policyName, "policy", "P", "none", "set CPU manager Policy")
+	pflag.StringVarP(&podSpecPath, "pod-spec", "p", "", "pod spec path")
+	pflag.Parse()
+
+	reservedCPUSet := parseReservedCPUsOrDie(rawReservedCPUs)
+	params := cpumgrx.Params{
+		PolicyName:     policyName,
+		Hint:           parseHintOrDie(rawHint),
+		ReservedCPUSet: reservedCPUSet,
+		ReservedCPUQty: resource.MustParse(fmt.Sprintf("%d", reservedCPUSet.Size())),
+	}
+
 	mgrx, err := cpumgrx.NewFromParams(params)
 	if err != nil {
 		klog.Errorf("cpumanager creation failed: %v", err)
 		os.Exit(1)
 	}
 
+	pod := readPodSpecOrDie(podSpecPath)
+
+	cpus, err := mgrx.Run(pod)
+	if err != nil {
+		klog.Errorf("cpumanager allocation failed: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s\n", cpus.String())
+}
+
+func parseHintOrDie(rawHint string) topologymanager.TopologyHint {
+	hints, err := tmutils.ParseGOHints([]string{rawHint})
+	if err != nil {
+		klog.Errorf("error parsing hint: %v", err)
+		os.Exit(1)
+	}
+	if len(hints) != 1 {
+		klog.Errorf("wrong hints given: %#v", hints)
+		os.Exit(1)
+	}
+	// slightly abuse because we cannot predict the key (do we really care about the key?)
+	for _, hint := range hints {
+		return hint[0]
+	}
+
+	// can't happen
+	return topologymanager.TopologyHint{}
+}
+
+func parseReservedCPUsOrDie(rawReservedCPUs string) cpuset.CPUSet {
+	reservedCPUs, err := cpuset.Parse(rawReservedCPUs)
+	if err != nil {
+		klog.Errorf("bad format for reserved CPU set: %v", err)
+		os.Exit(1)
+	}
+	return reservedCPUs
+}
+
+func readMachineInfoOrDie(machineInfoPath string) *cadvisorapi.MachineInfo {
+	var machineInfo cadvisorapi.MachineInfo
+	src, err := os.Open(machineInfoPath)
+	if err != nil {
+		klog.Errorf("error opening %q: %v", machineInfoPath, err)
+		os.Exit(1)
+	}
+	defer src.Close()
+
+	dec := json.NewDecoder(src)
+	if err := dec.Decode(&machineInfo); err != nil {
+		klog.Errorf("error decoding %q: %v", machineInfoPath, err)
+		os.Exit(1)
+	}
+
+	return &machineInfo
+}
+
+func readPodSpecOrDie(podSpecPath string) *v1.Pod {
+	var pod v1.Pod
+	src, err := os.Open(podSpecPath)
+	if err != nil {
+		klog.Errorf("error opening %q: %v", podSpecPath, err)
+		os.Exit(1)
+	}
+	defer src.Close()
+
+	dec := json.NewDecoder(src)
+	if err := dec.Decode(&pod); err != nil {
+		klog.Errorf("error decoding %q: %v", podSpecPath, err)
+		os.Exit(1)
+	}
+
+	return &pod
 }
